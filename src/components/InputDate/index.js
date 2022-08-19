bbf9e172ae4109b4e09e4421498274ce544a9519
acepnurmansidik <acepnurmansidik@gmail.com>
2022-08-20 03:57:43 +0700

    added component

---
+++ src/components/InputDate/index.js
@@ -0,0 +1,40 @@
+import React, { useEffect, useRef } from "react";
+import { DateRange } from "react-date-range";
+
+export default function IndexDate({ date, onChangeDate, setIsShowed }) {
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  });
+
+  const refDate = useRef(null);
+  const handleClickOutside = (event) => {
+    if (refDate && !refDate.current.contains(event.target)) {
+      setIsShowed(false);
+    }
+  };
+
+  const check = (focus) => {
+    focus.indexOf(1) < 0 && setIsShowed(false);
+  };
+
+  return (
+    <div
+      className="position-absolute"
+      // style={{ top: '59px' }}
+      style={{ zIndex: "1" }}
+      ref={refDate}
+    >
+      <DateRange
+        editableDateInputs={true}
+        onChange={onChangeDate}
+        moveRangeOnFirstSelection={false}
+        onRangeFocusChange={check}
+        ranges={[date]}
+        maxDate={new Date()}
+      />
+    </div>
+  );
+}
